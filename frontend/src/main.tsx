9bb2659d8f4a02917a01fb39af3d43c0b4262d3e
Valentin Lerouge <cloudd2vi@gmail.com>
2024-10-17 08:01:20 +0200

    add client auery provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,18 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { ThemeProvider } from "@/components/theme/provider";
-import App from "./App.tsx";
-import "./index.css";
+import { ThemeProvider } from '@/components/theme/provider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ThemeProvider>
-  </StrictMode>
+  </StrictMode>,
 );
