83e71962e560b84e8cc7eacab4aa57a67ca485b9
DopeCloudd <cloudd2vi@gmail.com>
2024-10-21 13:52:54 +0200

    change content invoice

--- frontend/src/pages/invoice-create.tsx
+++ frontend/src/pages/invoice-create.tsx
@@ -48,14 +48,43 @@ export function InvoiceCreate() {
       <div className="p-6 bg-background">
         <h2 className="text-2xl font-bold mb-4">Créer une facture</h2>
         <div className="grid gap-4">
-          <div>
-            <Label htmlFor="company-name">Nom de l'entreprise</Label>
-            <Input id="company-name" type="text" value={companyName} onChange={e => setCompanyName(e.target.value)} placeholder="Nom de l'entreprise" />
+          <div className="w-full grid grid-cols-2 gap-2">
+            <div>
+              <Label htmlFor="company-name">
+                Nom de votre entreprise<span className="text-red-600">*</span>
+              </Label>
+              <Input id="company-name" type="text" value={companyName} onChange={e => setCompanyName(e.target.value)} placeholder="Nom de votre entreprise" />
+            </div>
+            <div>
+              <Label htmlFor="company-name">SIRET de votre entreprise</Label>
+              <Input id="company-name" type="text" value={companyName} onChange={e => setCompanyName(e.target.value)} placeholder="SIRET de votre entreprise" />
+            </div>
           </div>
 
           <div>
-            <Label htmlFor="client-name">Nom du client</Label>
+            <Label htmlFor="company-name">Adresse de votre entreprise</Label>
+            <Input id="company-name" type="text" value={companyName} onChange={e => setCompanyName(e.target.value)} placeholder="Adresse de votre entreprise" />
+          </div>
+
+          <div className="w-full grid grid-cols-2 gap-2">
+            <div>
+              <Label htmlFor="client-name">Nom de l'entité / personne à facturer</Label>
+              <Input id="client-name" type="text" value={clientName} onChange={e => setClientName(e.target.value)} placeholder="Nom du client" />
+            </div>
+            <div>
+              <Label htmlFor="client-name">SIRET de l'entité à facturer</Label>
+              <Input id="client-name" type="text" value={clientName} onChange={e => setClientName(e.target.value)} placeholder="Nom du client" />
+            </div>
+          </div>
+
+          <div>
+            <Label htmlFor="client-name">Adresse de l'entité / personne à facturer</Label>
             <Input id="client-name" type="text" value={clientName} onChange={e => setClientName(e.target.value)} placeholder="Nom du client" />
+          </div>
+
+          <div>
+            <Label htmlFor="invoice-date">Référence de la facture</Label>
+            <Input id="invoice-date" type="text" placeholder="Référence de la facture" value={invoiceDate} onChange={e => setInvoiceDate(e.target.value)} />
           </div>
 
           <div>
@@ -80,8 +109,8 @@ export function InvoiceCreate() {
 
       {/* Aperçu de la facture à droite */}
       <div className="flex justify-center items-center h-full w-full bg-muted">
-        <div className="p-6 bg-foreground shadow-lg h-5/6 w-5/6 text-black">
-          <div className="mb-6">
+        <div className="p-6 bg-foreground shadow-lg h-5/6 w-5/6 text-black flex flex-col">
+          <div className="mb-2">
             <h3 className="text-lg font-semibold"> Nom de votre entreprise : {companyName}</h3>
             <p>SIRET : {clientName}</p>
             <p>Adresse : {invoiceDate}</p>
@@ -91,6 +120,11 @@ export function InvoiceCreate() {
             <h3 className="text-lg font-semibold">Nom de l'entité / personne : {companyName}</h3>
             <p>SIRET : {clientName}</p>
             <p>Adresse : {invoiceDate}</p>
+          </div>
+
+          <div className="mb-4 text-right">
+            <p>Référence : {clientName}</p>
+            <p>Date : {invoiceDate}</p>
           </div>
 
           <table className="w-full mb-4">
@@ -118,8 +152,29 @@ export function InvoiceCreate() {
             </tbody>
           </table>
 
-          <div className="text-right font-bold">
-            <p>Total: {calculateTotal()} €</p>
+          <div className="text-right font-bold mt-4">
+            <p className="w-fit ms-auto">Total: {calculateTotal()} €</p>
+          </div>
+
+          <div>
+            <p>Lu et approuvé</p>
+            <p>Le : </p>
+            <p>Signature : </p>
+          </div>
+
+          <div className="mt-auto flex flex-col">
+            <span className="border border-t border-muted w-full my-4"></span>
+            <div className="w-full flex justify-between">
+              <div className="flex flex-col">
+                <span className="text-xs">Nom de votre entreprise</span>
+                <span className="text-xs">SIRET : </span>
+              </div>
+              <div className="flex flex-col">
+                <span className="text-xs">Email : </span>
+                <span className="text-xs">Tél. : </span>
+                <span className="text-xs">Site web : </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
