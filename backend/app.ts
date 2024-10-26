ece94952acead6f0237820f8efa72de8654ec1ef
Valentin Lerouge <cloudd2vi@gmail.com>
2024-10-26 17:59:37 +0200

    change @types/express due to conflict

--- backend/app.ts
+++ backend/app.ts
@@ -1,13 +1,28 @@
 import { errorHandler } from "@middlewares/errorMiddleware";
 import { notFoundHandler } from "@middlewares/notFoundMiddleware";
 import userRoutes from "@routes/userRoutes";
+import bodyParser from "body-parser";
+import compression from "compression";
+import cors from "cors";
 import * as dotenv from "dotenv";
-import express from "express";
+import express, { Application } from "express";
+import helmet from "helmet";
 
 dotenv.config();
 
-const app = express();
-app.use(express.json());
+const app: Application = express();
+// Configuration CORS
+const corsOptions = {
+  origin: "*", // Autoriser ces deux origines
+  methods: ["GET", "POST", "PUT", "DELETE"], // Les méthodes autorisées
+  credentials: true, // Autorise l'envoi de cookies ou de tokens d'authentification
+  allowedHeaders: ["Content-Type", "Authorization"], // Autorise ces headers spécifiques
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(helmet());
+app.use(compression());
 
 // Définir les routes
 app.use("/api/users", userRoutes);
